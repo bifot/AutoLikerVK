ce75e579d547962cda992ac149dc1f3724e37335
bifot <bifot@bifot.ru>
2017-04-05 12:42:07 +0300

    v2.0

--- app.js
+++ app.js
@@ -1,45 +1,62 @@
-var request = require('request');
+const request = require('request');
 
-var token = 'TOKEN';
-var profile = 'bifot';
-var album = 'saved';
+const token = 'your_profile_token';
+const profile = 'bifot';
+const album = 'saved';
 
-request(`https://api.vk.com/method/users.get?user_ids=${profile}&v=5.8`, (error, response, body) => {
-  if (!error && response.statusCode == 200) {
-    var json = JSON.parse(body);
-    var id = json.response[0].id;
+request({
+  url: 'https://api.vk.com/method/users.get',
+  method: 'POST',
+  form: {
+    user_ids: profile,
+    v: 5.8
+  },
+  json: true
+}, (err, res, body) => {
+  if (!err && res.statusCode == 200) {
+    const id = body.response[0].id;
 
-    request(`https://api.vk.com/method/photos.get?owner_id=${id}&rev=1&album_id=${album}&count=1000&v=5.60`, (error, response, body) => {
-      if (!error && response.statusCode == 200) {
-        var json = JSON.parse(body);
-        var items = json.response.items;
+    request({
+      url: 'https://api.vk.com/method/photos.get',
+      method: 'POST',
+      form: {
+        owner_id: id,
+        rev: 1,
+        album_id: album,
+        count: 1000,
+        access_token: token,
+        v: 5.60
+      },
+      json: true
+    }, (err, res, body) => {
+      if (!err && res.statusCode == 200) {
+        const items = body.response.items;
 
-        items.forEach((photo, index) => {
-          let photoId = photo.id;
-
+        items.forEach((photo, i) => {
           setTimeout(() => {
-            request(`https://api.vk.com/method/likes.add?type=photo&owner_id=${id}&item_id=${photoId}&access_token=${token}&v=5.60`, (error, response, body) => {
-              if (!error && response.statusCode == 200) {
-                var json = JSON.parse(body);
-                var photoURL = `https://vk.com/photo${id}_${photoId}`;
-
-                if (json.response) {
-                  console.log(`Лайк поставлен. Всего лайков на фотографии: ${json.response.likes}`);
-                  console.log(`Фотография => ${photoURL}`);
+            request({
+              url: 'https://api.vk.com/method/likes.add',
+              method: 'POST',
+              form: {
+                type: 'photo',
+                owner_id: id,
+                item_id: photo.id,
+                access_token: token,
+                v: 5.60
+              },
+              json: true
+            }, (err, res, body) => {
+              if (!err && res.statusCode == 200) {
+                if (body.response) {
+                  console.log(`https://vk.com/photo${id}_${photo.id}`, body);
                 } else {
-                  console.log(`Ошибка: ${json.error.error_msg}`);
+                  console.log('Ошибка:', body.error.error_msg);
                 }
-              } else {
-                console.log('Не получилось поставить лайк.');
               }
             });
-          }, index * 3000);
+          }, i * 3000);
         });
-      } else {
-        console.log('Ошибка при получении фотографий.')
       }
     });
-  } else {
-    console.log('Ошибка при получении id юзера.');
   }
 });
